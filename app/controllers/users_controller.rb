667b4f2d4e6685b6c3b2133d83304db4603e7088
シンプルシェイプ <iamryoman@gmail.com>
2022-04-04 13:34:51 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototype = Prototype.all
+    @prototype = Prototype.where(user_id: @user.id)
   end
 
 
